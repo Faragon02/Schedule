31095743e3508597286d22f1429565459d99e9c1
Faragon02 <chj20202@naver.com>
2025-04-03 01:39:32 +0900

    move to dto -> dto/sign

--- src/main/java/com/example/schedule/members/dto/UpdatePasswordRequestDto.java
+++ src/main/java/com/example/schedule/members/dto/UpdatePasswordRequestDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 @Getter
 public class UpdatePasswordRequestDto {
     private final String oldPassword;
-
     private final String newPassword;
 
     public UpdatePasswordRequestDto(String oldPassword, String newPassword){
