90c4d810bfadd66c02991c0216c5a6da83542e60
Faragon02 <chj20202@naver.com>
2025-04-04 03:55:26 +0900

    feat : custom and Basic Exception

--- src/main/java/com/example/schedule/exception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.example.schedule.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
